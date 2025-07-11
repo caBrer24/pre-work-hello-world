042368427c47f81d61713fa7f331d16c7a1f5cf9
Carlos Cabrera <carlos.cabrecar@gmail.com>
2025-07-10 22:12:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/preworkhelloworld/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.preworkhelloworld
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private var switchImg: Boolean = true
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        changeNuggetImg()
+        val button = findViewById<Button>(R.id.helloButton)
+        setupButton(button)
+    }
+
+
+    private fun setupButton(button: Button) {
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello back!", Toast.LENGTH_SHORT).show()
+
+            if (switchImg) {
+                changeNuggetImg()
+                switchImg = false
+            } else {
+                changeNuggetImgBack()
+                switchImg = true
+
+            }
+        }
+    }
+
+    private fun changeNuggetImg() {
+        val imageView = findViewById<ImageView>(R.id.nuggetImg)
+        imageView.setImageResource(R.drawable.ls20250710204245)
+
+    }
+
+    private fun changeNuggetImgBack() {
+        val imageView = findViewById<ImageView>(R.id.nuggetImg)
+        imageView.setImageResource(R.drawable.img_20250710_203847)
+
+    }
+}
